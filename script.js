579e188f1753a7bb63f70ff2579160849e7caff9
Harshwardhan <harshawardhandudhal@gmail.com>
2026-03-08 22:18:42 +0530

    added themes and logic for converting currencies

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ const lightThemeButton = document.getElementById("lightThemeButton");
 const darkThemeButton = document.getElementById("darkThemeButton");
 const darkThemeIcon = document.querySelector(".darkThemeButton i");
 const lightThemeIcon = document.querySelector(".lightThemeButton i");
-const currencyInput = document.getElementById("currencyInput");
-const convertButton = document.getElementById("convertButton");
-const outputField = document.getElementById("outputField");
 
+// Dark Theme Listener.
 darkThemeButton.addEventListener("click", function () {
   darkThemeButton.classList.add("showAnimation");
 
@@ -15,11 +13,14 @@ darkThemeButton.addEventListener("click", function () {
   lightThemeButton.style.background = "black";
   lightThemeIcon.style.color = "white";
 
+  document.body.classList.toggle("darkTheme");
+
   darkThemeButton.addEventListener("animationend", function () {
     darkThemeButton.classList.remove("showAnimation");
   });
 });
 
+// Light Theme Listener.
 lightThemeButton.addEventListener("click", function () {
   lightThemeButton.classList.add("showAnimation");
 
@@ -28,15 +29,25 @@ lightThemeButton.addEventListener("click", function () {
 
   darkThemeButton.style.background = "black";
   darkThemeIcon.style.color = "white";
+  document.body.classList.toggle("darkTheme");
 
   lightThemeButton.addEventListener("animationend", function () {
     lightThemeButton.classList.remove("showAnimation");
   });
 });
 
+const convertButton = document.getElementById("convertButton");
+const outputField = document.getElementById("outputField");
 convertButton.addEventListener("click", function () {
-  let currency = 0;
-  currency = currencyInput.value;
-
-  outputField.innerHTML = currency;
+  const currencyInput = document.getElementById("currencyInput").value;
+  const fromCurrency = document.getElementById("fromCurrency").value;
+  const toCurrency = document.getElementById("toCurrency").value;
+  let rates = {
+    USD: 1,
+    INR: 83,
+    AED: 0.82
+  };
+  let amount = (currencyInput / rates[fromCurrency]) * rates[toCurrency];
+  outputField.innerText="";
+  outputField.innerText=amount.toFixed(2);
 });
