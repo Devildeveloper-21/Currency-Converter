f84041197c1cb1f3f9cba5771b320f373de0b8d7
Harshwardhan <harshawardhandudhal@gmail.com>
2026-03-19 22:59:23 +0530

    added error handling and api fetch convert function

--- script.js
+++ script.js
@@ -36,18 +36,33 @@ lightThemeButton.addEventListener("click", function () {
   });
 });
 
+let rate;
 const convertButton = document.getElementById("convertButton");
 const outputField = document.getElementById("outputField");
 convertButton.addEventListener("click", function () {
-  const currencyInput = document.getElementById("currencyInput").value;
   const fromCurrency = document.getElementById("fromCurrency").value;
   const toCurrency = document.getElementById("toCurrency").value;
-  let rates = {
-    USD: 1,
-    INR: 83,
-    AED: 0.82
-  };
-  let amount = (currencyInput / rates[fromCurrency]) * rates[toCurrency];
-  outputField.innerText="";
-  outputField.innerText=amount.toFixed(2);
+  getCurrencyInfo(fromCurrency, toCurrency);
 });
+
+function convertCurrency() {
+  const currencyInput = Number(document.getElementById("currencyInput").value);
+  let amount = currencyInput * rate;
+  outputField.innerText = "";
+  outputField.innerText = amount.toFixed(2);
+}
+
+async function getCurrencyInfo(getFromCurrency, getToCurrency) {
+  try {
+    const res = await fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${getFromCurrency}.json`,
+    )
+      const data = await res.json();
+      rate= data[getFromCurrency][getToCurrency];
+      convertCurrency();
+    
+  } catch (error) {
+    console.log("Data fetch failed",error);
+    
+  }
+}
